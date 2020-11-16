eec205fdda5556386f65fe6dec0033c449761756
JuanValdez <juantvaldez85@gmail.com>
2020-11-16 16:29:37 -0500

    feat: started header

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <nav className="nav-container">
+        <div className="brand-logo">LOGO</div>
+
+        <div className="links-container">
+          <h5>Green Team</h5>
+          <h5>Start Smart</h5>
+          <h5>Community</h5>
+          <h5 className="contact-container">Contact Us</h5>
+        </div>
+      </nav>
+    </div>
+  );
 }
 
 export default App;
