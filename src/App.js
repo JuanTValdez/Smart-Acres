91cc3c1d3c88842ac01a9217e9999a27ae845e70
JuanValdez <juantvaldez85@gmail.com>
2020-11-18 16:42:07 -0500

    added: Evolution section

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import "./App.css";
+import lettucePlate from "./images/lettuceplate_sml.png";
 
 function App() {
   return (
@@ -13,6 +14,43 @@ function App() {
           <h5 className="contact-container">Contact Us</h5>
         </div>
       </nav>
+
+      <div className="intro-container">
+        <div className="headline-container">
+          <h1 className="headline-txt">
+            We're growing the UAE's food system and the way you eat.
+          </h1>{" "}
+          <h4>
+            Our food system is in dire need of help and now's our chance to fix
+            it.
+          </h4>
+          <button className="tell-me-more"> TELL ME MORE!</button>
+        </div>
+        <img src={lettucePlate} alt="Lettuce"></img>
+      </div>
+
+      {/*  */}
+      <div className="think-container">
+        <div className="left-think-box">
+          <h5>
+            {" "}
+            <span className="txt-underline">THI</span>NK OUTSIDE THE BOX, THINK
+            INSIDE THE CROPS
+          </h5>
+          <h2>The Evolution of Agriculture</h2>
+        </div>
+        <div className="right-think-box">
+          <p>
+            Hello, lettuce lovers! You're here because just like us your
+            passionate about high-quality local foods that are healthy,
+            sustainable and long-term economical.
+            <br />
+            <br />
+            We're constantly think of ways to provide you with the happiest
+            experience while enjoying our crisp and super-fresh greens!.
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
